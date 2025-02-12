3b665792037cea46f24c9c7f9f790baa74f1f1a4
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-02-12 10:17:27 +0900

    [feat] apikey 안전을 위해 엔드포인트 추가

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -5,15 +7,19 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// CORS 설정
+const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
+
 app.use(cors());
+app.use(express.static(path.join(__dirname, "docs"))); // 정적 파일 제공
 
-// 정적 파일 경로 설정
-app.use(express.static(__dirname));
+// API 키를 클라이언트에 전달하는 엔드포인트
+app.get("/api/key", (req, res) => {
+  res.json({ clientId: NAVER_CLIENT_ID });
+});
 
-// 기본 라우팅 설정
+// 기본 라우트 설정
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "tourMaps.html"));
+  res.sendFile(path.join(__dirname, "docs", "tourMaps.html"));
 });
 
 app.listen(port, () => {
