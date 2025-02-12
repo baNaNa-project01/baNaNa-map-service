455ab305149cb9277a329a31a57fdd7404e94db9
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-02-12 11:03:17 +0900

    [feat] 마커 기능 확인용
    테스트용 마커 추가

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,6 @@
+// 지도 객체를 저장할 전역 변수
+let map;
+
 // 서버에서 API 키 가져오기
 fetch("/api/key")
   .then((response) => response.json())
@@ -16,10 +19,28 @@ fetch("/api/key")
   })
   .catch((error) => console.error("Failed to load API key:", error));
 
+// 지도 생성
 function initMap() {
   const mapOptions = {
     center: new naver.maps.LatLng(37.3595704, 127.105399),
     zoom: 10,
   };
-  new naver.maps.Map("map", mapOptions);
+
+  // 지도 객체 저장
+  map = new naver.maps.Map("map", mapOptions);
+
+  // 마커 추가
+  addMarker(37.3595704, 127.105399);
+}
+
+// 마커 추가 함수
+function addMarker(lat, lng) {
+  new naver.maps.Marker({
+    map: map,
+    position: new naver.maps.LatLng(lat, lng),
+    icon: {
+      content: "<div class='marker'></div>", // 따옴표 오류 수정
+      anchor: new naver.maps.Point(15, 15), // 마커 중심 조정
+    },
+  });
 }
